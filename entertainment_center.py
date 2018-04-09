4a1836c30553f6f8ff91b03db1135e5bd9c97d8d
William Howell <William@leahs-air.home>
2018-04-08 20:57:11 -0400

    Updates to format per pep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,48 @@
 import media
 import fresh_tomatoes
 
-#create a series of Movie instances 
-toy_story = media.Movie("Toy Story", "1hr 21min", "Some story line", "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-avatar = media.Movie("Avatar", "2hr 42min", "Some other planet movie", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-boondock_saints = media.Movie("Boondock Saints", "1hr 48min", "Brother who avenge in the name of God","https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/The_Boondock_Saints_poster.jpeg/220px-The_Boondock_Saints_poster.jpeg","https://www.youtube.com/watch?v=ydXojYfCF3I")
-chisum = media.Movie("Chisum", "1hr 51min", "Man who avenges and protects", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/Chisum1.jpg/220px-Chisum1.jpg", "https://www.youtube.com/watch?v=1p9r03roFaw")
-man_on_fire = media.Movie("Man on Fire", "1hr 53min", "Bodyguard risks all to save little girl", "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Man_on_fireposter.jpg/220px-Man_on_fireposter.jpg", "https://www.youtube.com/watch?v=eDDh50B6kA4")
-kleine_arschloch = media.Movie("Das Kleine Arschloch", "1hr 22min", "Sotry of a troublesome young man", "https://ia.media-imdb.com/images/M/MV5BMTI1MjkxMjUwMl5BMl5BanBnXkFtZTcwNzAxNjkyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=jhYbV_7ryPo")
+# create a series of Movie instances
+toy_story = media.Movie(
+    "Toy Story",
+    "1hr 21min",
+    "Some story line",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie(
+    "Avatar",
+    "2hr 42min",
+    "Some other planet movie",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+boondock_saints = media.Movie(
+    "Boondock Saints",
+    "1hr 48min",
+    "Brother who avenge in the name of God",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/The_Boondock_Saints_poster.jpeg/220px-The_Boondock_Saints_poster.jpeg",
+    "https://www.youtube.com/watch?v=ydXojYfCF3I")
+
+chisum = media.Movie(
+    "Chisum",
+    "1hr 51min",
+    "Man who avenges and protects",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/Chisum1.jpg/220px-Chisum1.jpg",
+    "https://www.youtube.com/watch?v=1p9r03roFaw")
+
+man_on_fire = media.Movie(
+    "Man on Fire",
+    "1hr 53min",
+    "Bodyguard risks all to save little girl",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Man_on_fireposter.jpg/220px-Man_on_fireposter.jpg",
+    "https://www.youtube.com/watch?v=eDDh50B6kA4")
+
+kleine_arschloch = media.Movie(
+    "Das Kleine Arschloch",
+    "1hr 22min",
+    "Story of a troublesome young man",
+    "https://ia.media-imdb.com/images/M/MV5BMTI1MjkxMjUwMl5BMl5BanBnXkFtZTcwNzAxNjkyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    "https://www.youtube.com/watch?v=jhYbV_7ryPo")
 
 # create an array of Movie instances to provide as an argument for fresh_tomatoes.open_movies()
 movies = [toy_story, avatar, boondock_saints, chisum, man_on_fire, kleine_arschloch]
--- media.py
+++ media.py
@@ -1,25 +1,18 @@
-
-
 # Create Parent Class for movies and other video types
 class Video():
-    #initialize Video Class
+    # Initialize Video Class
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
 
 
-#Pass Video as argument to inherit class Video attributes
+# Pass Video as argument to inherit class Video attributes
 class Movie(Video):
-    
-    def __init__(self, title, duration, movie_storyline, poster_image, trailer_youtube):
-        #initialize Parent class for inheritance
+    def __init__(self, title, duration, movie_storyline,
+                 poster_image, trailer_youtube):
+        # Initialize Parent class for inheritance
         Video.__init__(self, title, duration)
-        #assign class Movie attributes
+        # Assign class Movie attributes
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-
-
-    
-        
